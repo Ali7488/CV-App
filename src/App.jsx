4c588ea5cc7ef1005625061874b3cd790a0976c4
Ali7488 <mohannad.eali@gmail.com>
2026-08-04 18:20:41 -0600

    added basic website plan and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import "./App.css";
+import "./styles/App.css";
 
-function App() {}
+function App() {
+  return (
+    <>
+      {" "}
+      <header>
+        <h1>CV Generator</h1>
+      </header>
+      <main></main>
+      <footer>
+        <p>
+          Built by:{" "}
+          <a href="https:/github.com/Ali7488" target="_blank">
+            Mohannad Ali
+          </a>
+        </p>
+      </footer>
+    </>
+  );
+}
 
 export default App;
