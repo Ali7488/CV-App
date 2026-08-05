c5d6959f04fb6b801af7a25567129032fcabdd35
Ali7488 <mohannad.eali@gmail.com>
2026-08-05 12:40:23 -0600

    added base layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,21 @@ import "./styles/App.css";
 function App() {
   return (
     <>
-      {" "}
       <header>
         <h1>CV Generator</h1>
       </header>
-      <main></main>
+      <main>
+        <div className="workspace"></div>
+      </main>
       <footer>
         <p>
           Built by:{" "}
-          <a href="https:/github.com/Ali7488" target="_blank">
+          <a
+            href="https://github.com/Ali7488"
+            target="_blank"
+            className="footer-link"
+            rel="noopener noreferrer"
+          >
             Mohannad Ali
           </a>
         </p>
